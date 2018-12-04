3fd67c65521bc4ec29a2cb03334564afbb5b382c
CodingYuanLiu <lqyuan980413@163.com>
2018-12-04 11:00:06 -0500

    start implementing codegen

--- lab6/codegen.c
+++ lab6/codegen.c
@@ -11,6 +11,33 @@
 #include "codegen.h"
 #include "table.h"
 
+//caller_saved register: rax rdi rsi rdx rcx r8 r9 r10 r11
+//callee_saved register: rbx rbp r12 r13 r14 r15 
+
+AS_instrList iList = NULL,last = NULL;
+
 //Lab 6: put your code here
+static void emit(AS_instr inst)
+{
+    if(iList)
+    {
+        last->tail = AS_InstrList(inst,NULL);
+        last = last->tail;
+    }
+    else
+    {
+        iList = AS_InstrList(inst,NULL);
+        last = iList;
+    }
+}
+
+
 AS_instrList F_codegen(F_frame f, T_stmList stmList) {
+    AS_instrList list;
+    
+}
+
+void savecalleeregs()
+{
+    
 }
--- lab6/codegen.h
+++ lab6/codegen.h
@@ -3,4 +3,9 @@
 
 AS_instrList F_codegen(F_frame f, T_stmList stmList);
 
+//Save or restore callee registers to temp.
+void savecalleeregs();
+void restorecalleeregs();
+
+static void emit(AS_instr inst);
 #endif
